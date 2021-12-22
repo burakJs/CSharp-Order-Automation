15434d432c7800bd610a654cb0d40196bab1ad39
burakJs <burakimdat13@gmail.com>
2021-12-22 14:39:38 +0300

    added item class

---
+++ Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGMOrderAutomation.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+        public int quantity { get; set; }
+        public float price { get; set; }
+        public int companyId { get; set; }
+
+        public Item(int id, String name, int quantity, float price, int companyId)
+        {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+            this.price = price;
+            this.companyId = companyId;
+        }
+    }
+}
